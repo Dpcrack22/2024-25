2a98c1eff068a15f32152bc69024216e9fb09c2a
Dpcrack22 <davidperera2006@gmail.com>
2025-02-05 19:44:27 +0100

    e

--- JAVA/src/MiPrimerPaquete/Matriz.java
+++ JAVA/src/MiPrimerPaquete/Matriz.java
@@ -5,22 +5,6 @@ import java.util.Random;
 public class Matriz {
 
 	public static void main(String[] args) {
-        int[][] matriz = new int[10][5];
-        Random rand = new Random();
-
-        // Llenar la matriz con valores aleatorios
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 5; j++) {
-                matriz[i][j] = rand.nextInt(100) + 1; // Números entre 1 y 100
-            }
-        }
-
-        // Imprimir la matriz
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(matriz[i][j] + "\t");
-            }
-            System.out.println();
-        }
-}
+		
+	}
 }
